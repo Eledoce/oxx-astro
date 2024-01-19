b2d492b9d9453c11cfbed4fea90d534de2255305
Angel Ramirez <miguelangel8877@outlook.com>
2024-01-18 23:30:34 -0600

    responsive desing and products page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat: ['Montserrat Variable', 'sans-serif'],
+        Roboto: ['Roboto', 'sans-serif'],
+        Inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
